de4dd8d2036369476d5acbc79a52b7aa2c6a24d7
Literally Void <literallyvoid@gmail.com>
2021-02-11 12:51:51 -0800

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+set -e
+
+cd "$(git rev-parse --show-toplevel)"
+
+if ! git diff HEAD --exit-code --quiet; then
+    echo "dirty! please commit changes first"
+    exit 1
+fi
+
+./build.sh
+
+COMMIT="$(git rev-parse --short HEAD)"
+
+cd _out/
+
+git add -A
+
+git commit -m 'Deploy $COMMIT'
